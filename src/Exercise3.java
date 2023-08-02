c9f567e7eb51229038ff55ac556f09d64325d144
lemon <lemon.juice9341@gmail.com>
2023-08-02 17:46:52 +0300

    This class print the sum of three-digit number

--- src/Exercise3.java
+++ src/Exercise3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Exercise_3 {
+public class Exercise3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number: ");
