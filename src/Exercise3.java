cd8ff0569a2d53550f8797d5adf007a33ef7aac1
lemon <lemon.juice9341@gmail.com>
2023-08-06 21:29:29 +0300

    This class print the sum of three-digit number

--- src/Exercise3.java
+++ src/Exercise3.java
@@ -4,21 +4,21 @@ public class Exercise3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number: ");
-        int Number = scanner.nextInt();
-        int Sum = 0, Cycle = 0;
-        int Num = Number;
-        while (Num > 0) {
-            int Percent = Num % 10;
-            Cycle++;
-            Num /= 10;
+        int number = scanner.nextInt();
+        int sum = 0, cycle = 0;
+        int num = number;
+        while (num > 0) {
+            int Percent = num % 10;
+            cycle++;
+            num /= 10;
         }
-        if (Cycle == 3) {
-            while (Number > 0) {
-                int Percent = Number % 10;
-                Sum += Percent;
-                Number /= 10;
+        if (cycle == 3) {
+            while (number > 0) {
+                int Percent = number % 10;
+                sum += Percent;
+                number /= 10;
             }
-            System.out.println("The sum is: " + Sum);
+            System.out.println("The sum is: " + sum);
         } else {
             System.out.println("You did enter a three-digit number");
         }
